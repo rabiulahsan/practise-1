5745882af7cb8a03c7c5e65afbdf8e345cc303fc
rabiul-ahsan <ra.rabiulahsan@gmail.com>
2023-03-30 13:28:24 +0600

    cart quantity aded

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,15 +2,25 @@ import React from "react";
 import "./Cart.css";
 
 const Cart = (props) => {
-  const { cart } = props;
+  // const cart = props.cart; // option 1
+  const { cart } = props; // option 2
+
+  // console.log(cart);
 
   let total = 0;
   let shippingTotal = 0;
-
+  let quantity = 0;
   for (const product of cart) {
-    total = total + product.price;
+    // if(product.quantity === 0){
+    //     product.quantity = 1;
+    // }
+    // product.quantity = product.quantity || 1;
+
+    total = total + product.price * product.quantity;
     shippingTotal = shippingTotal + product.shipping;
+    quantity = quantity + product.quantity;
   }
+
   const tax = parseFloat((total * 0.08).toFixed(2));
   const grandTotal = (total + shippingTotal + tax).toFixed(2);
 
